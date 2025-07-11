309c0490a575d2dd5646690bf21843a5c6351501
Siphiwe Msibi <siphiwe.msibi@live.com>
2025-07-11 14:17:56 +0200

    Create an application form class and processed the data using cleaned_data method

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            # Process the data in form.cleaned_data
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            date = form.cleaned_data['date']
+            occupation = form.cleaned_data['occupation']
+
+            print(first_name)
+            
+
     return render(request, 'index.html')
